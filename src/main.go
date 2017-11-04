3dfec100838ffeeded875c1a6b90eb0f8e3f0549
Carlos <cdelatorre@correo.ugr.es>
2017-11-04 12:06:21 +0100

    Primer metodo para el webservice
    This reverts commit 8a1c87c94c544eb34ef3434086f690b9e5ba0e9a.

--- src/main.go
+++ src/main.go
@@ -2,16 +2,38 @@ package main
 
 import (
     "fmt"
-    "html"
     "log"
+    "os"
     "net/http"
+
+    "github.com/gorilla/mux"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-    })
 
-    log.Fatal(http.ListenAndServe(":8080", nil))
+    router := mux.NewRouter().StrictSlash(true)
+    router.HandleFunc("/", Exit)
+    router.HandleFunc("/index", Index)
+    router.HandleFunc("/todos", TodoIndex)
+    router.HandleFunc("/todos/{todoId}", TodoShow)
 
+    log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func Exit(w http.ResponseWriter, r *http.Request) {
+    os.Exit(1)
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Welcome!")
+}
+
+func TodoIndex(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Todo Index!")
+}
+
+func TodoShow(w http.ResponseWriter, r *http.Request) {
+    vars := mux.Vars(r)
+    todoId := vars["todoId"]
+    fmt.Fprintln(w, "Todo show:", todoId)
 }
