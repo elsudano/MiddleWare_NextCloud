1eaf6df713736b2c61ff6373075d2df995ccd746
Carlos <cdelatorre@correo.ugr.es>
2017-12-10 12:13:39 +0100

    dividimos el codigo en diferentes ficheros para que sea mas visible

--- webservice/handlers.go
+++ webservice/handlers.go
@@ -1,56 +1,18 @@
 package webservice
 
 import (
-	"crypto/tls"
 	"encoding/json"
-	"encoding/xml"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
-
+	"strings"
 	"github.com/gorilla/mux"
 )
 
 // DEBUG es una constante que indica que se activa el modo depuración
 // a lo largo de todo el codigo hay diferentes mensajes de pantalla
 // para poder realizar un rustico depurado de la aplicación.
-const DEBUG bool = false
-
-// Esta es la dirección URL donde tenemos alojado nuestro servidor de NextCloud.
-//
-// Para realizar el despliegue automatico tenemos que tener en cuenta que debe
-// haber una variable de entorno que se llame URL_BASE y que indique cual es
-// la URL a donde se realizaran las peticiones para NextCloud.
-var URL_BASE = os.Getenv("URL_BASE")
-
-// Esta variable se usa para saber cual será el metodo por defecto que
-// se utilizará para realizar las peticiones a NextCloud.
-//
-// Entre las opciones que tenemos destacan:
-//
-// PROPFIND, GET
-//
-// Las demas opciones las podemos ver en la documentación de NextCloud.
-// https://docs.nextcloud.com/server/12/developer_manual/client_apis/WebDAV/index.html
-var METHOD = "PROPFIND"
-
-// Como estamos desarrollando una API que se encaga de comunicarnos con
-// NextCloud, y la propia API de NextCloud se accede a través de un cliente
-// web necesitamos generar un cliente en nuestra API para poder realizar
-// las peticiones.
-//
-// Por eso con esta variable creamos un cliente el cual nos permite realizar
-// peticiones sobre paginas https pero sin la necesidad de la verificaión
-// del certificado de la pagina, esto se hace por si la pagina tiene un
-// certificado auto-firmado.
-var NET_CLIENT = &http.Client{
-	Transport: &http.Transport{
-		TLSClientConfig: &tls.Config{
-			InsecureSkipVerify: true,
-		},
-	},
-}
+const DEBUG bool = true
 
 // FStatus función que se encarga de responder a la disponibilidad que
 // tiene nuestro webservice, siempre respondera de la misma manera,
@@ -63,104 +25,74 @@ func FStatus(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(status)
 }
 
-// FList funcion que se encarga de
+// FList funcion que se encarga de listar todos los eventos de NextCloud
 func FList(w http.ResponseWriter, r *http.Request) {
-	if DEBUG {
-		fmt.Println("Hacemos la peticion de la lista")
-		fmt.Println("La URL es: " + URL_BASE)
-	}
-	peticion, err := http.NewRequest(METHOD, URL_BASE, nil)
-	if err != nil {
-		if DEBUG {
-			fmt.Println("Este es el error de no encuentro la URL")
-		}
-		panic(err)
-	}
-	peticion.SetBasicAuth(os.Getenv("USER_NEXTCLOUD"), os.Getenv("PASS_NEXTCLOUD"))
-	peticion.Header.Set("Cache-Control", "no-cache")
-	peticion.Header.Set("Content-Type", "application/xml")
-	peticion.Header.Set("Charset", "utf-8")
-
-	respuesta, err := NET_CLIENT.Do(peticion)
-	if err != nil {
-		if DEBUG {
-			fmt.Println("Este es el error de que no tiene usuario ni pass")
-		}
-		panic(err)
-	}
-
-	if respuesta.StatusCode == http.StatusMultiStatus {
-		bodyRaw, err := ioutil.ReadAll(respuesta.Body)
-		//bodyString := string(bodyRaw)
-		//fmt.Println("Resultado de la peticion a la WEB\n" + bodyString)
-		if err != nil {
-			if DEBUG {
-				fmt.Println("Error en pasar la respuesta a string")
-			}
-			panic(err)
-		}
-
-		// Esta es la estructura de XML que vamos a usar para almacenar
-		// temporalmente los datos
-		var xmlData Multistatus
-		// Esta es la estructura de datos que vamos a enviar en formato
-		// JSON al cliente, es una colección de objetos de JSON
-		var responseJSON []JSONObject
-		// Este es un evento de calandario en formato JSON
-		var eventJSON JSONObject
-
-		// en esta linea montamos un xml para luego poder navegar por el
-		if err := xml.Unmarshal(bodyRaw, &xmlData); err != nil {
-			fmt.Println("Este error es del XML decodificando")
-			panic(err)
-		}
-		// con esto recorremos todo el XML
-		for _, respTag := range xmlData.Responses {
-			eventJSON = JSONObject{
-				Id:       respTag.Propstat[0].Prop.Etag,
-				Href:     respTag.Href,
-				Name:     respTag.Propstat[0].Prop.Name,
+	// Esta es la estructura de datos que vamos a enviar en formato
+	// JSON al cliente, es una colección de objetos de JSON
+	var responseJSON []EventJSON
+	// Este es un evento de calandario en formato JSON
+	var eventJSON EventJSON
+	// con esto recorremos todo el XML
+	// menos el primer Response que es donde estan los datos
+	// del calendario al que accedemos
+	for i, respTag := range xmlData.Responses {
+		// en esta parte mostramos el resultado del JSON y formateamos la
+		// salida por consola de un objeto tipo evento
+		// evitamos el 0 por que son los datos del calendario completo
+		if i != 0 && respTag.Propstat[0].Status == STATUSOK &&
+		   strings.Contains(respTag.Propstat[0].Prop.Content_Type, "component=vevent") {
+			eventJSON = EventJSON {
+				// Con esta linea eliminamos todo lo que sobra para quedarnos
+				// solo con el ID del evento
+				Id: strings.TrimSuffix(strings.TrimPrefix(respTag.Href, URL_BASE+"Nextcloud-"), ".ics"),
+				// Lo que hacemos con replace es quitar las comillas que molestan
+				// en el campo ETAG que vienen por defecto en el XML
+				Etag:     strings.Replace(respTag.Propstat[0].Prop.Etag, "\"", "", -1),
 				Modified: respTag.Propstat[0].Prop.Modified,
 			}
+			// añadimos el evento a la respuesta que vamos a dar en JSON
 			responseJSON = append(responseJSON, eventJSON)
-			// con esto imprimimos por consola el resultado de XML
-			if DEBUG {
-				fmt.Println("URL: ", respTag.Href)
-				for _, propstatTag := range respTag.Propstat {
-					fmt.Println("\tStatus: ", propstatTag.Status)
-					fmt.Println("\tName: ", propstatTag.Prop.Name)
-					fmt.Println("\tContent-Type: ", propstatTag.Prop.Content_Type)
-					fmt.Println("\tSize: ", propstatTag.Prop.Size)
-					fmt.Println("\tModified: ", propstatTag.Prop.Modified)
-					fmt.Println("\tEtag: ", propstatTag.Prop.Etag)
-					for _, propTag := range propstatTag.Prop.PropList {
-						fmt.Println("\t\tXMLName:", propTag.XMLName, "->", propTag.Value)
-					}
-					fmt.Println()
-				}
-				fmt.Println()
-			}
-		}
-		// Con esto rellenamos el JSON que vamos a devolver al cliente
-		if err := json.NewEncoder(w).Encode(responseJSON); err != nil {
-			if DEBUG {
-				fmt.Println("Este error es del JSON")
-			}
-			panic(err)
-		}
-	} else {
-		if DEBUG {
-			fmt.Println("El codigo de estado para la respuesta es:", respuesta.StatusCode)
 		}
 	}
-	defer respuesta.Body.Close()
+	// Con esto rellenamos el JSON que vamos a devolver al cliente
+	if err := json.NewEncoder(w).Encode(responseJSON); err != nil {
+		if DEBUG {
+			fmt.Println("Este error es del JSON")
+		}
+		panic(err)
+	}
+	if DEBUG {
+		printXML(xmlData)
+	}
 }
 
 // se llama cuando se accede a "/show/{id}"
 func FShow(w http.ResponseWriter, r *http.Request) {
+	// recogemos las variables de la URI
 	vars := mux.Vars(r)
 	Id := vars["id"]
-	json.NewEncoder(w).Encode(Id)
+	// Este es un evento de calandario en formato JSON
+	var eventJSON icsJSON
+
+	if DEBUG {
+		fmt.Println("Estas son las variables de entrada:", vars)
+	}
+	// con esto recorremos todo el XML
+	for i , respTag := range xmlData.Responses {
+		if i != 0 && strings.TrimSuffix(strings.TrimPrefix(respTag.Href, URL_BASE+"Nextcloud-"), ".ics") == Id {
+			stringICS := readICS(eventJSON, URL_COMPLETE_PATH + "Nextcloud-" + Id + ".ics")
+			if DEBUG {
+				fmt.Println("Este es el contenido del fichero ICS\n", stringICS)
+			}
+		}
+	}
+	// Con esto rellenamos el JSON que vamos a devolver al cliente
+	if err := json.NewEncoder(w).Encode(eventJSON); err != nil {
+		if DEBUG {
+			fmt.Println("Este error es del JSON")
+		}
+		panic(err)
+	}
 }
 
 // se llama cuando se accede a "/new"
@@ -178,6 +110,11 @@ func FDelete(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// se llama cuando se accede a "/refresh"
+// Se usa para refrescar la base de datos temporal
+func FRefresh(w http.ResponseWriter, r *http.Request) {
+	xmlData = readXML()
+}
 // se llama cuando se accede a "/exit"
 func FExit(w http.ResponseWriter, r *http.Request) {
 	os.Exit(0)
---
+++ webservice/utils.go
@@ -0,0 +1,248 @@
+package webservice
+
+import (
+	"crypto/tls"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+
+	//"github.com/apognu/gocal"
+)
+
+// Como estamos desarrollando una API que se encaga de comunicarnos con
+// NextCloud, y la propia API de NextCloud se accede a través de un cliente
+// web necesitamos generar un cliente en nuestra API para poder realizar
+// las peticiones.
+//
+// Por eso con esta variable creamos un cliente el cual nos permite realizar
+// peticiones sobre paginas https pero sin la necesidad de la verificaión
+// del certificado de la pagina, esto se hace por si la pagina tiene un
+// certificado auto-firmado.
+var NET_CLIENT = &http.Client{
+	Transport: &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	},
+}
+
+// Estructura de datos que se encarga de mantener la lectura del XML para que
+// nos sirva como base de datos temporal
+var xmlData = readXML()
+
+// readXML Con esta función leemos directamente desde una fuente WebDAV un calendario
+// y lo comvertimos a una estructura XML que podemos leer despues desde otras
+// funcionalidades para poder presentar la información de otras maneras.
+//
+// El formato en el que se devuelve este XML viene dado por una serie de
+// estructuras que se han definido en el fichero xmlobject.go
+func readXML() Multistatus {
+	if DEBUG {
+		fmt.Println("Hacemos la peticion de la lista")
+		fmt.Println("La URL Completa es: " + URL_COMPLETE_PATH)
+	}
+	// Esta es la estructura de XML que vamos a usar para almacenar
+	// temporalmente los datos
+	var xmlData Multistatus
+	// se almacena la petición que se realiza a la URL_COMPLETE_PATH y
+	// un err por si falla la petición
+	peticion, err := http.NewRequest("PROPFIND", URL_COMPLETE_PATH, nil)
+	if err != nil {
+		if DEBUG {
+			fmt.Println("Este es el error de no encuentro la URL")
+		}
+		panic(err)
+	}
+	// como no da ningun error se procede a setear las diferentes cabeceras
+	// y demas parametros de la petición
+	peticion.SetBasicAuth(os.Getenv("USER_NEXTCLOUD"), os.Getenv("PASS_NEXTCLOUD"))
+	peticion.Header.Set("Cache-Control", "no-cache")
+	peticion.Header.Set("Content-Type", "application/xml")
+	peticion.Header.Set("Charset", "utf-8")
+	// al igual que la solicitud tambien se hace con la respuesta
+	// se genera un err por si falla dicha respuesta
+	respuesta, err := NET_CLIENT.Do(peticion)
+	if err != nil {
+		if DEBUG {
+			fmt.Println("Este es el error de que no tiene usuario ni pass")
+		}
+		panic(err)
+	}
+	// normalmente en los WebDAV el estado devuelto suele ser MultiEstado
+	// en http://sabre.io/dav/building-a-caldav-client/ se puede ver
+	if respuesta.StatusCode == http.StatusMultiStatus {
+		bodyRaw, err := ioutil.ReadAll(respuesta.Body)
+		if err != nil {
+			if DEBUG {
+				fmt.Println("Error en pasar la respuesta a string")
+			}
+			panic(err)
+		}
+		// en esta linea montamos un xml para luego poder navegar por el
+		if err := xml.Unmarshal(bodyRaw, &xmlData); err != nil {
+			fmt.Println("Este error es del XML decodificando")
+			panic(err)
+		}
+	} else if DEBUG {
+		fmt.Println("El codigo de estado para la respuesta es:", respuesta.StatusCode)
+	}
+	defer respuesta.Body.Close()
+	return xmlData
+}
+
+// readICS Con esta función leemos el fichero ICS desde una ruta de internet
+// y lo devolvemos en un formato JSON
+//
+// Para que se pueda realizar cualquier operación con el fichero descargado
+// se devuelve una cadena y de esa manera se puede parsear o guardar.
+func readICS(JSONstruct icsJSON, icsFILE string) string {
+	if DEBUG {
+		fmt.Println("El evento es: " + icsFILE)
+	}
+	// se almacena la petición que se realiza a la URL_COMPLETE_PATH y
+	// un err por si falla la petición
+	peticion, err := http.NewRequest("GET", icsFILE, nil)
+	if err != nil {
+		if DEBUG {
+			fmt.Println("Este es el error de no encuentro la URL")
+		}
+		panic(err)
+	}
+	// como no da ningun error se procede a setear las diferentes cabeceras
+	// y demas parametros de la petición
+	peticion.SetBasicAuth(os.Getenv("USER_NEXTCLOUD"), os.Getenv("PASS_NEXTCLOUD"))
+	peticion.Header.Set("Cache-Control", "no-cache")
+	peticion.Header.Set("Content-Type", "text/calendar; charset=utf-8; component=vevent")
+	peticion.Header.Set("Charset", "utf-8")
+	// al igual que la solicitud tambien se hace con la respuesta
+	// se genera un err por si falla dicha respuesta
+	respuesta, err := NET_CLIENT.Do(peticion)
+	if err != nil {
+		panic(err)
+	}
+	// datos en bruto del fichero descargado
+	fileRaw, err := ioutil.ReadAll(respuesta.Body)
+	if err != nil {
+		panic(err)
+	}
+	// Creamos el fichero temporal
+	tmpfile, err := ioutil.TempFile("", "vevent")
+	if err != nil {
+		panic(err)
+	}
+	defer os.Remove(tmpfile.Name()) // lo preparamos para ser borrado
+	// Escribimos en el fichero temporal el que nos descargamos
+	if _, err := tmpfile.Write(fileRaw); err != nil {
+		panic(err)
+	}
+	// Leemos el fichero temporal
+	tmpcontent, err := ioutil.ReadFile(tmpfile.Name())
+	if err != nil {
+		panic(err)
+	}
+	// Cerramos el fichero temporal para que se borre
+	if err := tmpfile.Close(); err != nil {
+		panic(err)
+	}
+	defer respuesta.Body.Close()
+	return string(tmpcontent)
+}
+
+// print
+// printXML Con esta funcion podemos imprimir el contenido del XML que pasamos
+// por parametros.
+//
+// Esta función solo imprime por pantalla el contenido del mismo.
+func printXML(xmlData Multistatus) {
+	for i, respTag := range xmlData.Responses {
+		if i == 0 {
+			// El response 0 es la posición donde están los datos del calendario
+			fmt.Println("Esta es la información del calendario\nURL:", respTag.Href)
+			for _, propstatTag := range respTag.Propstat {
+				fmt.Println("\tStatus:", propstatTag.Status)
+				fmt.Println("\tObject Display Name:", propstatTag.Prop.Name)
+				fmt.Println("\tPosition:", propstatTag.Prop.Position)
+				fmt.Println("\tColor:", propstatTag.Prop.Color)
+				// le quitamos 1 por que el primero es el registro del calendario
+				fmt.Println("\tNumero de Eventos:", propstatTag.Prop.NEvents-1)
+				fmt.Println("\tOwner Principal:", propstatTag.Prop.Owner)
+				fmt.Println("\tOwner Display Name:", propstatTag.Prop.OwnerDN)
+				fmt.Println("\tResourceType List:")
+				for _, ResoTag := range propstatTag.Prop.ResoList {
+					fmt.Println("\t\tCollection:", ResoTag.Collection)
+					fmt.Println("\t\tCalendar:", ResoTag.Calendar)
+				}
+				fmt.Println("\tSupport Calendar Component Set:", propstatTag.Prop.SupCalComSet)
+				fmt.Println("\tShedule Calendar Transport:", propstatTag.Prop.SheduCalTran)
+				fmt.Println("\tOthers Visible options:")
+				for _, propTag := range propstatTag.Prop.PropList {
+					fmt.Println("\t\tXMLName:", propTag.XMLName, "->", propTag.Value)
+				}
+				fmt.Println()
+			}
+			fmt.Println()
+		} else if respTag.Propstat[0].Status == STATUSOK && strings.Contains(respTag.Propstat[0].Prop.Content_Type, "component=vevent") {
+			// en esta parte mostramos el resultado del JSON y formateamos la
+			// salida por consola de un objeto tipo evento
+			fmt.Println("Esta es la información de un evento\nURL:", respTag.Href)
+			for _, propstatTag := range respTag.Propstat {
+				if propstatTag.Status == STATUSOK {
+					fmt.Println("\tEsto es un prop con Status:", propstatTag.Status)
+					fmt.Println("\tContent-Type:", propstatTag.Prop.Content_Type)
+					fmt.Println("\tSize:", propstatTag.Prop.Size)
+					fmt.Println("\tModified:", propstatTag.Prop.Modified)
+					// le quitamos 1 por que el primero es el registro del calendario
+					fmt.Println("\tEtag:", strings.Replace(propstatTag.Prop.Etag, "\"", "", -1))
+					fmt.Println("\tResourceType List:")
+					for _, ResoTag := range propstatTag.Prop.ResoList {
+						fmt.Println("\t\tCollection:", ResoTag.Collection)
+						fmt.Println("\t\tCalendar:", ResoTag.Calendar)
+					}
+				} else {
+					fmt.Println("\tEsto es un prop con Status:", propstatTag.Status)
+					fmt.Println("\tOthers Visible options:")
+					for _, propTag := range propstatTag.Prop.PropList {
+						fmt.Println("\t\tXMLName:", propTag.XMLName, "->", propTag.Value)
+					}
+				}
+
+			}
+			fmt.Println()
+		} else {
+			// en esta parte imprimimos todos los demas elementos
+			// del XML que no hemos contemplado arriba
+			fmt.Println("Esto es algo que no estaba contemplado\nURL:", respTag.Href)
+			for _, propstatTag := range respTag.Propstat {
+				fmt.Println("\tStatus:", propstatTag.Status)
+				fmt.Println("\tObject Display Name:", propstatTag.Prop.Name)
+				fmt.Println("\tContent-Type:", propstatTag.Prop.Content_Type)
+				fmt.Println("\tSize:", propstatTag.Prop.Size)
+				fmt.Println("\tPosition:", propstatTag.Prop.Position)
+				fmt.Println("\tColor:", propstatTag.Prop.Color)
+				fmt.Println("\tModified:", propstatTag.Prop.Modified)
+				// le quitamos 1 por que el primero es el registro del calendario
+				fmt.Println("\tNumero de Eventos:", propstatTag.Prop.NEvents-1)
+				fmt.Println("\tEtag:", strings.Replace(propstatTag.Prop.Etag, "\"", "", -1))
+				fmt.Println("\tOwner Principal:", propstatTag.Prop.Owner)
+				fmt.Println("\tOwner Display Name:", propstatTag.Prop.OwnerDN)
+				fmt.Println()
+				fmt.Println("\tResourceType List:")
+				for _, ResoTag := range propstatTag.Prop.ResoList {
+					fmt.Println("\t\tCollection:", ResoTag.Collection)
+					fmt.Println("\t\tCalendar:", ResoTag.Calendar)
+				}
+				fmt.Println("\tSupport Calendar Component Set:", propstatTag.Prop.SupCalComSet)
+				fmt.Println("\tShedule Calendar Transport:", propstatTag.Prop.SheduCalTran)
+				fmt.Println("\tOthers Visible options:")
+				for _, propTag := range propstatTag.Prop.PropList {
+					fmt.Println("\t\tXMLName:", propTag.XMLName, "->", propTag.Value)
+				}
+				fmt.Println()
+			}
+			fmt.Println()
+		}
+	}
+}
